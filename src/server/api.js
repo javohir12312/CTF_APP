383362ea0c74bd88caeb5c2409e8ecf37939201d
javohir12312 <abduxalilovjavohir393@gmail.com>
2024-04-27 23:34:51 +0500

    change error

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.168.100.19:5003";
-const Forimage = "http://192.168.100.19:5003";
+axios.defaults.baseURL = "http://localhost:5003";
+const Forimage = "http://localhost:5003";
 
 export { axios, Forimage };
